e3c0a403d6388cb79cffde78274420baad23934e
honghoker <sinbein9@gmail.com>
2024-03-11 14:12:49 +0900

    FIX Ensure compilation on macOS by restricting platform-specific features
    This commit introduces conditional availability checks to ensure that
    the application can be compiled and run on macOS environments. It explicitly
    marks certain features as unavailable on macOS, while allowing them on iOS
    and tvOS platforms. This approach maintains cross-platform compatibility
    and adheres to platform-specific guidelines, ensuring a seamless development
    experience across different operating systems.

--- Source/SwiftUI additions/SVGKFastImageViewSUI.swift
+++ Source/SwiftUI additions/SVGKFastImageViewSUI.swift
@@ -2,30 +2,57 @@
 import SwiftUI
 import SVGKit
 
+#if !os(macOS)
 @available(iOS 13.0, tvOS 13.0, *)
-struct SVGKFastImageViewSUI:UIViewRepresentable
-{
-    @Binding var url:URL
-    @Binding var iconSize:CGFloat
-    
-    func makeUIView(context: Context) -> SVGKFastImageView {
+struct SVGKFastImageViewSUI: UIViewRepresentable {
+  @Binding private var url: URL
+  @Binding private var iconSize: CGFloat
 
-        let svgImage = SVGKImage(contentsOf: url)
-        return SVGKFastImageView(svgkImage: svgImage ?? SVGKImage())
-        
-    }
-    func updateUIView(_ uiView: SVGKFastImageView, context: Context) {
-        uiView.image = SVGKImage(contentsOf: url)
-        
-        uiView.image.size = CGSize(width: iconSize,height: iconSize)
-    }
-    
-    
+  init(url: Binding<URL>, iconSize: Binding<CGFloat>) {
+    self._url = url
+    self._iconSize = iconSize
+  }
+
+  func makeUIView(context: Self.Context) -> SVGKFastImageView {
+    let svgImage = SVGKImage(contentsOf: url)
+    return SVGKFastImageView(svgkImage: svgImage ?? SVGKImage())
+  }
+
+  func updateUIView(_ uiView: SVGKFastImageView, context: Self.Context) {
+    uiView.image = SVGKImage(contentsOf: url)
+    uiView.image.size = CGSize(width: iconSize, height: iconSize)
+  }
 }
+#elseif os(macOS)
+@available(macOS 10.15, *)
+struct SVGKFastImageViewSUI: NSViewRepresentable {
+  @Binding private var url: URL
+  @Binding private var iconSize: CGFloat
 
-@available(iOS 13.0, tvOS 13.0, *)
+  init(url: Binding<URL>, iconSize: Binding<CGFloat>) {
+    self._url = url
+    self._iconSize = iconSize
+  }
+
+  func makeNSView(context: Self.Context) -> SVGKFastImageView {
+    let svgImage = SVGKImage(contentsOf: url)
+    return SVGKFastImageView(svgkImage: svgImage ?? SVGKImage())
+  }
+
+  func updateNSView(_ nsView: SVGKFastImageView, context: Self.Context) {
+    nsView.image = SVGKImage(contentsOf: url)
+    nsView.image.size = CGSize(width: iconSize, height: iconSize)
+  }
+}
+#endif
+
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, *)
 struct SVGImage_Previews: PreviewProvider {
     static var previews: some View {
-        SVGKFastImageViewSUI(url: .constant(URL(string:"https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/android.svg")!), iconSize: .constant(50.0))
+        SVGKFastImageViewSUI(
+          url: .constant(URL(string:"https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/android.svg")!),
+          iconSize: .constant(50.0)
+        )
     }
 }
